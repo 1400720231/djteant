a60437a9d35bc83dbc93106dd8af488c8d05f7f7
panda <xxx#@.com>
2020-07-30 20:49:59 +0800

    django多租户系统研究，初次提交

---
+++ user/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+from django_multitenant.fields import *
+from django_multitenant.models import *
+
+
+class Store(TenantModel):
+    tenant_id = 'id'
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=255)
+    email = models.CharField(max_length=50)
+
+
+class Product(TenantModel):
+    store = models.ForeignKey(Store, on_delete=models.PROTECT)
+    tenant_id = 'store_id'
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta(object):
+        unique_together = ["id", "store"]
+
+
+class Purchase(TenantModel):
+    store = models.ForeignKey(Store, on_delete=models.PROTECT)
+    tenant_id = 'store_id'
+    product_purchased = TenantForeignKey(Product, on_delete=models.PROTECT)
